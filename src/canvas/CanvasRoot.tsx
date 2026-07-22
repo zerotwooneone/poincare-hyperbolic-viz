b7d02628d818f2a561d1954fc5113af0318bdebb
zero wsl <squiredd@gmail.com>
2026-07-22 11:19:25 -0700

    mount visual components and activate lazy camera auto-rotation controls

--- src/canvas/CanvasRoot.tsx
+++ src/canvas/CanvasRoot.tsx
@@ -1,17 +1,26 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import { DiskBackground } from './scene/DiskBackground';
+import { ExplorerMesh } from './actors/ExplorerMesh';
 
 export const CanvasRoot: React.FC = () => {
   return (
     <div style={{ width: '100vw', height: '100vh', background: '#050508' }}>
-      <Canvas camera={{ position: [0, 0, 2.5], fov: 60 }}>
-        <ambientLight intensity={0.2} />
-        <pointLight position={[10, 10, 10]} />
+      <Canvas camera={{ position: [0, 0, 1.8], fov: 60 }}>
+        <ambientLight intensity={0.4} />
+        <pointLight position={[5, 5, 5]} intensity={1.5} />
         <Suspense fallback={null}>
-          {/* Geometries & Actors will be injected here */}
+          <DiskBackground />
+          <ExplorerMesh />
         </Suspense>
-        <OrbitControls enableZoom={true} maxPolarAngle={Math.PI / 2} minPolarAngle={0} />
+        <OrbitControls 
+          enableZoom={true} 
+          maxPolarAngle={Math.PI / 2.5} 
+          minPolarAngle={0} 
+          autoRotate 
+          autoRotateSpeed={0.3} 
+        />
       </Canvas>
     </div>
   );
