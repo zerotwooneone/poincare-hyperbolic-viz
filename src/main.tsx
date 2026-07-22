ca0bd9fc145fa0dfe139f82f52bb96692f4fbdf7
zero <squiredd@gmail.com>
2026-07-22 11:49:59 -0700

    add entry points

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import { CanvasRoot } from './canvas/CanvasRoot';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <CanvasRoot />
   </React.StrictMode>
 );
